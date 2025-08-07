0673ea55e77f3393062e0b7ec94daea609770545
Forpgm <phm.giamy@gmail.com>
2025-08-07 20:24:31 +0700

    cors & helmet

--- src/index.ts
+++ src/index.ts
@@ -8,11 +8,12 @@ import { defaultErrorHandler } from './middlewares/error.middlewares'
 import { initFolder } from './utils/file'
 import { UPLOAD_IMAGE_DIR, UPLOAD_VIDEO_DIR } from './constants/dir'
 import staticRouter from './routes/static.routes'
-import { MongoClient } from 'mongodb'
 import tweetsRouter from './routes/tweets.routes'
 import bookmarksRouter from './routes/bookmarks.routes'
 import likeRoutes from './routes/likes.routes'
 import searchRouter from './routes/search.routes'
+import helmet from 'helmet'
+import cors, { CorsOptions } from 'cors'
 
 const port = envConfig.port
 
@@ -30,6 +31,12 @@ databaseService.connect().then(() => {
 app.get('/', (req, res) => {
   res.send('hello world')
 })
+app.use(helmet())
+console.log(isProduction)
+const corsOption: CorsOptions = {
+  origin: isProduction ? envConfig.clientUrl : '*'
+}
+app.use(cors(corsOption))
 
 // http://localhost:3000/users/tweets
 app.use('/users', usersRouter) //route handler
@@ -40,6 +47,9 @@ app.use('/bookmarks', bookmarksRouter)
 app.use('/search', searchRouter)
 // app.use('/static/video', express.static(UPLOAD_VIDEO_DIR))
 app.use('/static', staticRouter)
+
+//app sd 1 error handler tổng
+app.use(defaultErrorHandler)
 
 app.listen(port, () => {
   console.log(`Project twitter này đang chạy trên post ${port}`)
